167ae53e70d1b93c04a36f99da17ea2c6c412a19
萤火科技 <635993292@qq.com>
2021-04-02 11:35:13 +0800

    优化：api地址改为兼容模式

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   // 系统名称
   name: "萤火商城2.0",
   // 必填: 后端api地址, 斜杠/结尾, 参照下面格式
-  // 例如: https://www.你的域名.com/index.php/api/
-  apiUrl: "https://www.你的域名.com/index.php/api/"
+  // 例如: https://www.你的域名.com/index.php?s=/api/
+  apiUrl: "https://www.你的域名.com/index.php?s=/api/"
 }
