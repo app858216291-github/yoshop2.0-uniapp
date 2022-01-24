8c50555584718660c1c74ada3274c9b2aaff9385
Yahoo <1808402272@qq.com>
2022-01-24 13:56:19 +0800

    修复富文本组件

--- components/page/richText/index.vue
+++ components/page/richText/index.vue
@@ -1,15 +1,13 @@
 <template>
   <!-- 富文本 -->
-  <view class="diy-richText" :style="{ padding: `${itemStyle.paddingTop}px ${itemStyle.paddingLeft}px`, background: itemStyle.background }">
-    <jyf-parser :html="params.content"></jyf-parser>
+  <view class="diy-richText"
+    :style="{ padding: `${itemStyle.paddingTop}px ${itemStyle.paddingLeft}px`, background: itemStyle.background }">
+    <mp-html :content="params.content" />
   </view>
 </template>
 
 <script>
-  import jyfParser from "@/components/jyf-parser/jyf-parser"
-
   export default {
-
     /**
      * 组件的属性列表
      * 用于组件自定义设置
@@ -18,11 +16,6 @@
       itemStyle: Object,
       params: Object
     },
-
-    components: {
-      jyfParser
-    },
-
     /**
      * 组件的方法列表
      * 更新属性和数据的方法与更新页面数据的方法类似
@@ -30,7 +23,6 @@
     methods: {
 
     }
-
   }
 </script>
 
